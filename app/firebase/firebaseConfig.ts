d7fde7b0533ca5b9803913bcc26255a36d0029da
chandu-0214 <ncr02141997@gmail.com>
2024-10-04 11:44:30 +0530

    firebase done

--- app/firebase/firebaseConfig.ts
+++ app/firebase/firebaseConfig.ts
@@ -11,7 +11,6 @@ const firebaseConfig = {
   appId: process.env.NEXT_PUBLIC_APPID,
   measurementId: process.env.NEXT_PUBLIC_MEASUREMENTID,
 };
-
 const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
 export const fireDB = getFirestore(app);
 export const firebaseAuth = getAuth(app);
